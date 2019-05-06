173adb108660e9cdbf5b82273adb8c3c86c948bf
Greatpan <38004594+Greatpanc@users.noreply.github.com>
2019-05-06 10:51:54 +0800

    Add files via upload

---
+++ YOLO_v1/data/ReadMe.md
@@ -0,0 +1,3 @@
+数据下载并解压在这个文件夹中就行了
+
+数据下载地址：https://pjreddie.com/media/files/VOCtrainval_06-Nov-2007.tar
---
+++ YOLO_v1/yolo/config.py
@@ -0,0 +1,47 @@
+# -*- coding:utf-8 -*-
+import os
+
+
+''' 配置文件(路径-数据集) '''
+DATA_PATH = 'data'
+PASCAL_PATH = os.path.join(DATA_PATH, 'pascal_voc')
+CACHE_PATH = os.path.join(PASCAL_PATH, 'cache')
+OUTPUT_DIR = os.path.join(PASCAL_PATH, 'output')   # 存放输出文件的地方，data/pascal_voc/output
+WEIGHTS_DIR = os.path.join(PASCAL_PATH, 'weights') # weights_dir, 路径为data/pascal_voc/weights
+WEIGHTS_FILE = None   # weights file
+# WEIGHTS_FILE = os.path.join(DATA_PATH, 'weights', 'YOLO_small.ckpt')
+
+CLASSES = ['aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus',
+           'car', 'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse',
+           'motorbike', 'person', 'pottedplant', 'sheep', 'sofa',
+           'train', 'tvmonitor']    # PASCAL VOC数据集的20个类别
+
+FLIPPED = True
+
+''' 模型图像及训练相关参数 '''
+IMAGE_SIZE = 448      # 输入图片的大小
+CELL_SIZE = 7         # 整张图片分为cell_size * cell_size的大小
+BOXES_PER_CELL = 2    # 每个cell负责预测两个bounding box
+ALPHA = 0.1     
+DISP_CONSOLE = False
+
+GPU = ''
+LEARNING_RATE = 0.0001   # 学习率
+DECAY_STEPS = 30000
+DECAY_RATE = 0.1
+STAIRCASE = True
+BATCH_SIZE = 45   # batch size
+MAX_ITER = 100     # 迭代次数，可自定义
+SUMMARY_ITER = 10
+SAVE_ITER = 10
+
+''' 损失函数参数（论文中涉及的参数） '''
+OBJECT_SCALE = 1.0
+NOOBJECT_SCALE = 1.0
+CLASS_SCALE = 2.0
+COORD_SCALE = 5.0
+
+''' 测试参数 '''
+THRESHOLD = 0.2
+IOU_THRESHOLD = 0.5   # IOU阈值0.5
+
